f387769bc43db63b96f0212c654a8cafd7471baf
NosbaoJ92 <joabson.nogueira@hotmail.com>
2023-08-09 13:26:21 -0300

    second commit

--- api/index.php
+++ api/index.php
@@ -73,13 +73,13 @@
         <div class="w-8/12 flex flex-wrap justify-center gap-2 mt-6 max-[767px]:w-full max-[767px]:gap-3 ">
             <?php
                 $cards = array(
-                    array('image' => 'img/earbuds.png', 'content' => 'Wireless Earbuds, IPX8', 'moeda' => 'R$', 'preco' => '89,90'),
-                    array('image' => 'img/headset.png', 'content' => 'Headset Logitech G233 Prodigy', 'moeda' => 'R$', 'preco' => '259,90'),
-                    array('image' => 'img/ps4.png', 'content' => 'Playstation 4 Slim', 'moeda' => 'R$', 'preco' => '3229,90'),
-                    array('image' => 'img/iphoneX.png', 'content' => 'Iphone X - Usado', 'moeda' => 'R$', 'preco' => '1.329,90'),
-                    array('image' => 'img/mouseG502.png', 'content' => 'Mouse Logitech G502', 'moeda' => 'R$', 'preco' => '129,90'),        
-                    array('image' => 'img/xiaomi.png', 'content' => 'Xiaomi Mi 1', 'moeda' => 'R$', 'preco' => '785,99'),        
-                    array('image' => 'img/notebook_xiaomi.png', 'content' => 'Xiaomi Mi Notebook Air 12.5', 'moeda' => 'R$', 'preco' => '5.785,99'),             
+                    array('image' => './api/img/earbuds.png', 'content' => 'Wireless Earbuds, IPX8', 'moeda' => 'R$', 'preco' => '89,90'),
+                    array('image' => './api/img/headset.png', 'content' => 'Headset Logitech G233 Prodigy', 'moeda' => 'R$', 'preco' => '259,90'),
+                    array('image' => './api/img/ps4.png', 'content' => 'Playstation 4 Slim', 'moeda' => 'R$', 'preco' => '3229,90'),
+                    array('image' => './api/img/iphoneX.png', 'content' => 'Iphone X - Usado', 'moeda' => 'R$', 'preco' => '1.329,90'),
+                    array('image' => './api/img/mouseG502.png', 'content' => 'Mouse Logitech G502', 'moeda' => 'R$', 'preco' => '129,90'),        
+                    array('image' => './api/img/xiaomi.png', 'content' => 'Xiaomi Mi 1', 'moeda' => 'R$', 'preco' => '785,99'),        
+                    array('image' => './api/img/notebook_xiaomi.png', 'content' => 'Xiaomi Mi Notebook Air 12.5', 'moeda' => 'R$', 'preco' => '5.785,99'),             
                 );
 
                 foreach ($cards as $index => $card) {
